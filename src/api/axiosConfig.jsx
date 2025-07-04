04acd5867796420851b0996e2c0eed907434db74
mirzaatifbaig <mirzaatifb91@gmail.com>
2025-07-04 17:31:38 +0500

    Appropriate changes

--- src/api/axiosConfig.jsx
+++ src/api/axiosConfig.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3000/",
   headers: {
     Accept: "application/json",
   },
