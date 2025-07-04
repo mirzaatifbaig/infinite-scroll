eaa9d9cfdd979fffed93b6362256445199be6104
mirzaatifbaig <mirzaatifb91@gmail.com>
2025-07-04 17:42:32 +0500

    Appropriate changes

--- json-server/api/index.js
+++ json-server/api/index.js
@@ -1,15 +1,22 @@
-import jsonServer from 'json-server';
+import express from 'express';
+import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, './db.json'));
-const middlewares = jsonServer.defaults();
+const app = express();
 
-server.use(middlewares);
-server.use(router);
+app.get('/', (req, res) => {
+  try {
+    const dbPath = path.join(__dirname, 'db.json');
+    const data = fs.readFileSync(dbPath, 'utf8');
+    res.json(JSON.parse(data));
+  } catch (error) {
+    console.error('Error reading db.json:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
-export default server;
+export default app;
